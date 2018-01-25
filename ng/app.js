c78bfa189e6991caa4f5a9bafca0c6accebf1126
nickagel <nagel@uvm.edu>
2018-01-24 19:22:38 -0500

    Remove hash/exclamation from route to create pretty urls

--- ng/app.js
+++ ng/app.js
@@ -1,16 +1,1 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "yellow.html"
-    })
-    .when("/red", {
-        templateUrl : "red.html"
-    })
-    .when("/green", {
-        templateUrl : "green.html"
-    })
-    .when("/blue", {
-        templateUrl : "blue.html"
-    });
-});
--- ng/route.js
+++ ng/route.js
@@ -1,15 +1,19 @@
-app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "templates/yellow.html"
-    })
-    .when("/red", {
-        templateUrl : "templates/red.html"
-    })
-    .when("/green", {
-        templateUrl : "templates/green.html"
-    })
-    .when("/blue", {
-        templateUrl : "templates/blue.html"
-    });
-});
+app.config(['$routeProvider','$locationProvider',
+    function ($routeProvider, $locationProvider){
+        $routeProvider
+        .when("/", {
+            templateUrl : "templates/yellow.html"
+        })
+        .when("/red", {
+            templateUrl : "templates/red.html"
+        })
+        .when("/green", {
+            templateUrl : "templates/green.html"
+        })
+        .when("/blue", {
+            templateUrl : "templates/blue.html"
+        });
+        $locationProvider.html5Mode(true);
+        $locationProvider.hashPrefix('!');
+    }]
+);
