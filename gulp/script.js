188f0a846ac3bba8bda9b18e0a7f57afa8b73b4b
nickagel <nagel@uvm.edu>
2018-01-24 19:58:35 -0500

    Materialize adding components

--- gulp/script.js
+++ gulp/script.js
@@ -4,7 +4,7 @@ var uglify = require('gulp-uglify')
 var ngAnnotate = require('gulp-ng-annotate')
 
 gulp.task('js-post', function () {
-  return gulp.src(["ng/app.js", "ng/route.js"])
+  return gulp.src(["ng/app.js", "ng/route.js", "js/materialize.js"])
     .pipe(concat("post-scripts.min.js"))
     .pipe(ngAnnotate())
     .pipe(uglify())
@@ -14,5 +14,7 @@ gulp.task('js-post', function () {
 gulp.task('js-pre', function () {
   return gulp.src(["js/angular.js","js/angular-routes.js"])
     .pipe(concat("pre-scripts.min.js"))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
     .pipe(gulp.dest("site_build"))
 })
