f4e0fe934ffdb2d325e375e1dcdb5e31f760aba4
nickagel <nagel@uvm.edu>
2018-02-05 21:48:20 -0500

    Tweaking

--- bin/bottom.php
+++ bin/bottom.php
@@ -1,3 +1,3 @@
-<script src="./post-scripts.min.js"></script>
+<script src="post-scripts.min.js"></script>
 </body>
 </html>
--- bin/top.php
+++ bin/top.php
@@ -12,8 +12,8 @@
         <meta name="author" content="Nick Agel">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link rel="icon" type="image/x-icon" href="./images/fav-icon.ico" />
-        <link rel="shortcut icon" href="./images/fav-icon.ico" />
-        <link rel="stylesheet" href="./style.min.css" type="text/css" media="screen">
-        <script src="./pre-scripts.min.js"></script>
+        <link rel="shortcut icon" href="images/fav-icon.ico" />
+        <link rel="stylesheet" href=".style.min.css" type="text/css" media="screen">
+        <script src="pre-scripts.min.js"></script>
 </head>
 <body ng-app="dataApp" ng-controller="dataCtrl">
