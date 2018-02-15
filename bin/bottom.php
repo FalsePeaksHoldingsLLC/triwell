60b3f0f8ec7df04a3f523225caa67ae1b7bea733
nickagel <nagel@uvm.edu>
2018-02-15 00:42:45 -0500

    Update triwell

--- bin/bottom.php
+++ bin/bottom.php
@@ -1,3 +1,17 @@
+<footer>
+    <ol>
+        <li>Copyright © 2018 TriWellRI, LLC. All Rights Reserved.</li>
+        <li><a href="SPECIALTHANKS">SPECIAL THANKS</a></li> 
+    </ol>
+    <ol>
+        <li><a href="DISCLAIMER">Disclaimer</a></li>
+        <li><a href="PRIVACYPOLICY">Privacy Policy</a></li>
+        <li><a href="TERMSOFSERVICE">Terms of Service</a></li>
+    </ol>
+    <p>
+        The information contained on this website is provided for general informational purposes only and is not intended to be medical advice. TriWellRI does not recommend any particular form of medical treatment or that persons manage their own health problems without the advice of a licensed health care practitioner. Information found or received through this website should not be used in place of a visit with, call to, consultation or advice from a health care provider. If you suspect you have a medical problem, or should you have any health care related questions, please promptly call or see your health care practitioner. 
+    </p>
+</footer>
 <script src="post-scripts.min.js"></script>
 </body>
 </html>
