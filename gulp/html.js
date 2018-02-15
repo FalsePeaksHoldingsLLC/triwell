fe3962512761d244941c2459bdbaf7b649929a9e
nickagel <nagel@uvm.edu>
2018-02-15 01:53:53 -0500

    update triwell

--- gulp/html.js
+++ gulp/html.js
@@ -3,7 +3,7 @@ var concat = require('gulp-concat')
 var htmlmin = require('gulp-htmlmin');
 
 gulp.task('html', function () {
-    return gulp.src(["bin/top.html", "bin/nav.html", "content/*", "bin/bottom.html"])
+    return gulp.src(["bin/top.html", "bin/layover.html", "bin/nav.html", "content/*", "bin/bottom.html"])
         .pipe(concat("index.html"))
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest("docs"));
